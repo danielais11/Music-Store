53f0d940410e6fa071f376df6441a7b6aa06a6b9
Daniel Aistleitner <daniel.aistleitner@htl-wels.at>
2026-06-12 12:22:16 +0200

    Improved design + Closebutton

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,11 +3,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     public static void main(String[] args) {
         launch(args);
@@ -17,8 +23,26 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../viewctrl/main.fxml"));
         Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
         scene.getStylesheets().add(getClass().getResource("../viewctrl/style.css").toExternalForm());        primaryStage.setScene(scene);
         primaryStage.setTitle("Music-Store");
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+
+        //Zum Draggen
+        root.setOnMousePressed((MouseEvent event) -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        // 3. Drag the stage on mouse drag
+        root.setOnMouseDragged((MouseEvent event) -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
+
+
         primaryStage.show();
     }
 }
